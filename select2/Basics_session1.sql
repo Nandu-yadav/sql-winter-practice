70c053f0cb2c5df929659a3df5d9a9bb7fd2c754
Nandu yadav <b210571@rgukt.ac.in>
2025-12-25 07:19:05 +0530

    Refine comments and formatting in SQL script
    Updated comments for clarity and formatting.

--- select2/Basics_session1.sql
+++ select2/Basics_session1.sql
@@ -1,20 +1,19 @@
 USE myDatabase
---ABOVE thing ensures that the data based used is the one wanted
+--ABOVE thing (the DataBase name) ensures that the data based used is the one wanted
 GO --written
-
 --this is  a comment
-
 /* this
 is
-a comment*/
-SELECT first_name,
-       country,
+a multiline comment*/
+SELECT first_name,     --column name
+       country,        -- column name
        score
-FROM customers
+FROM customers          -- Table name
 --WHERE  country = 'Germany' --AND score<500
 ORDER BY   country ASC,score ASC /* better to always add something*/
+
 --priority decides by written first
 --Nested sorting(ORDERED BY) seperated by commas
-
+-- ORDER BY is for sorting
 
 
